aba3c208fed51ab83e526aa29c42d78e859c71d4
Alexander Etxeberria <AlejandroEcheverria11@gmail.com>
2022-01-14 22:07:05 +0000

    Last XMonad Config

--- xmonad.hs
+++ xmonad.hs
@@ -1,32 +1,40 @@
 import XMonad
--- XMONAD + POLYBAR
-import XMonad.Hooks.ManageDocks
-import qualified XMonad.DBus as D
-import qualified DBus.Client as DC
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
--- ManageHook
-import XMonad.Hooks.ManageHelpers
 -- Ewmh Screen
 import XMonad.Hooks.EwmhDesktops
+-- ManageHook
+import XMonad.Hooks.ManageHelpers
 -- Key Binding
+import XMonad.Actions.CycleWS
+import XMonad.Actions.Navigation2D
+import XMonad.Util.EZConfig(additionalKeysP)
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
-import XMonad.Util.EZConfig(additionalKeysP)
-import XMonad.Actions.Navigation2D
 -- Layout
+import XMonad.Layout.EqualSpacing
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
+-- Xmonad + PolyBAR
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import qualified XMonad.DBus as D
+import qualified DBus.Client as DC
 
-myTerminal           = "alacritty" 
+myBorderWidth        = 4
 myClickJustFocuses :: Bool
 myClickJustFocuses   = True
-myBorderWidth        = 4
-myNormalBorderColor  = "#ffffff"
-myFocusedBorderColor = "#1414ff"
+myTerminal           = "alacritty" 
 myWorkspaces       :: [String]
-myWorkspaces         = ["I","II","III", "IV","V","VI","VII","VIII","IX"]
+myWorkspaces         = ["I","II","III", "IV","V","VI","VII","VIII"]
 
-myLayoutHook = avoidStruts (
+-- Colours
+myFocusedBorderColor = "#F5C035"
+myNormalBorderColor  = "#EEEEEE"
+gray      = "#7F7F7F"
+yellow    = "#F5C035"
+yellow2   = "#755B1A"
+
+myLayoutHook = avoidStruts $ equalSpacing 10 4 0 20 (
     tiled ||| Mirror tiled ||| Full
     )
   where
@@ -35,109 +43,132 @@ myLayoutHook = avoidStruts (
     ratio = 1/2
     delta = 3/100
 
-
 myManageHook :: ManageHook
 myManageHook = composeAll . concat $
-    [ [ className =? c --> doShift "I" | c <- myBrowsers   ]
-    , [ className =? b --> doShift "V" | b <- myGames      ]
-    , [ className =? "telegram-desktop" --> doShift "II"   ]
+    [ [ className =? b --> doShift "I"                     | b <- myBrowsers   ]
+    , [ className =? c --> doShift "II"                    | c <- myComs       ]
+    , [ className =? "Alacritty" --> doShift "III"   ]
+    , [ className =? "Deadbeef" --> doShift "IV"   ]
+    , [ className =? d --> doShift "V" | d <- myGames  ]
+    , [ className =? "Steam" --> doShift "VI"   ]
+    , [ className =? "Bitwarden" --> doShift "VIII"   ]
     , [ isFullscreen   --> doF W.focusDown <+> doFullFloat <+> hasBorder False ]
-    , [ isDialog       --> doFloat                         ]
+    , [ isDialog --> doFloat ]
+    , [ className =? g --> doCenterFloat | g <- myFloats                    ]
     ]
   where
-      myBrowsers = [ "qutebrowser", "falkon", "vivaldi-bin" ]
-      myGames    = [ "dota2", "clonehero", "Dwarf_Fortress" ]
-
--- Colours
-gray      = "#7F7F7F"
-gray2     = "#691A64"
-red       = "#900000"
-blue      = "#2E9AFE"
-white     = "#eeeeee"
+      myBrowsers = [ "qutebrowser", "Falkon", "Vivaldi-stable", "vivaldi" ]
+      myGames    = [ "dota2", "clonehero", "Dwarf_Fortress", "Blender" ]
+      myComs     = [ "TelegramDesktop", "Element", "discord" ]
+      myFloats   = [ "ranger" ]
 
 myLogHook :: DC.Client -> PP
 myLogHook dbus = def {
-    ppCurrent = wrap ("%{F" ++ blue ++ "} ") " %{F-}"
-  , ppVisible = wrap ("%{F" ++ gray2 ++ "} ") " %{F-}"
-  , ppUrgent = wrap ("%{F" ++ red ++ "} ") " %{F-}"
-  , ppTitle = shorten 30  
-  , ppHidden = wrap ("%{F" ++ gray2 ++ "} ") " %{F-}"
-  , ppOutput = D.send dbus
-  , ppHiddenNoWindows = wrap ("%{F" ++ gray ++ "} ") " %{F-}"
+    ppLayout    = shorten 30  
+    , ppCurrent = wrap ("%{F" ++ yellow ++ "} ") " %{F-}"
+    , ppTitle   = shorten 30  
+    , ppVisible = wrap ("%{F" ++ yellow2 ++ "} ") " %{F-}"
+    , ppUrgent  = wrap ("%{F" ++ yellow ++ "} ") " %{F-}"
+    , ppHidden  = wrap ("%{F" ++ yellow2 ++ "} ") " %{F-}"
+    , ppHiddenNoWindows = wrap ("%{F" ++ gray ++ "} ") " %{F-}"
+    , ppOutput  = D.send dbus
 }
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- launch a terminal
     [ ((modm,                xK_Return), spawn $ XMonad.terminal conf)
+
     -- launch rofi drun
     , ((modm,                xK_p     ), spawn "rofi -show drun")
+
     -- launch rofi -show 
     , ((modm .|. controlMask,xK_p     ), spawn "rofi -show")
+
     -- launch clipmenu
     , ((modm .|. shiftMask,  xK_p     ), spawn "clipmenu")
+
     -- close focused window
     , ((modm,                xK_q     ), kill)
+
     -- next layout
     , ((modm,                xK_space ), sendMessage NextLayout)
+
     -- move focus to the next window
     , ((modm,                xK_Tab   ), windows W.focusDown)
+
     -- push window back into tiling
     , ((modm,                xK_t     ), withFocused $ windows . W.sink)
+
     -- increment the number of windows in the master area
     , ((modm,                xK_comma ), sendMessage (IncMasterN 1))
+
     -- deincrement the number of windows in the master area
     , ((modm,                xK_period), sendMessage (IncMasterN (-1)))
+
     -- toggle the status bar gap
     , ((modm,                xK_b     ), sendMessage ToggleStruts)
+
     -- restart xmonad
     , ((modm .|. shiftMask,  xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+
     -- logout 
     , ((modm .|. controlMask,xK_q     ), spawn "loginctl terminate-user hibiscus-tea")
+
     -- Swap adjacent windows | no arrowkeys
-    , ((modm,                xK_l     ), windowSwap R True)
-    , ((modm,                xK_h     ), windowSwap L True)
-    , ((modm,                xK_k     ), windowSwap U True)
-    , ((modm,                xK_j     ), windowSwap D True)
+    , ((modm,                xK_l     ), windowGo   R True)
+    , ((modm,                xK_h     ), windowGo   L True)
+    , ((modm,                xK_k     ), windowGo   U True)
+    , ((modm,                xK_j     ), windowGo   D True)
+
     -- Directional navigation of windows | no arrowkeys   
-    , ((modm .|. shiftMask,  xK_l     ), windowGo R True)
-    , ((modm .|. shiftMask,  xK_h     ), windowGo L True)
-    , ((modm .|. shiftMask,  xK_k     ), windowGo U True)
-    , ((modm .|. shiftMask,  xK_j     ), windowGo D True)
+    , ((modm .|. shiftMask,  xK_l     ), windowSwap R True)
+    , ((modm .|. shiftMask,  xK_h     ), windowSwap L True)
+    , ((modm .|. shiftMask,  xK_k     ), windowSwap U True)
+    , ((modm .|. shiftMask,  xK_j     ), windowSwap D True)
+
     -- Swap adjacent windows
-    , ((modm,                xK_Right ), windowSwap R True)
-    , ((modm,                xK_Left  ), windowSwap L True)
-    , ((modm,                xK_Up    ), windowSwap U True)
-    , ((modm,                xK_Down  ), windowSwap D True)
+    , ((modm,                xK_Right ), windowGo   R True)
+    , ((modm,                xK_Left  ), windowGo   L True)
+    , ((modm,                xK_Up    ), windowGo   U True)
+    , ((modm,                xK_Down  ), windowGo   D True)
+
     -- Directional navigation of windows
-    , ((modm .|. shiftMask,  xK_Right ), windowGo R True)
-    , ((modm .|. shiftMask,  xK_Left  ), windowGo L True)
-    , ((modm .|. shiftMask,  xK_Up    ), windowGo U True)
-    , ((modm .|. shiftMask,  xK_Down  ), windowGo D True)
+    , ((modm .|. shiftMask,  xK_Right ), windowSwap R True)
+    , ((modm .|. shiftMask,  xK_Left  ), windowSwap L True)
+    , ((modm .|. shiftMask,  xK_Up    ), windowSwap U True)
+    , ((modm .|. shiftMask,  xK_Down  ), windowSwap D True)
+
+    , ((modm .|. controlMask,xK_l    ), nextWS)
+    , ((modm .|. controlMask,xK_h    ), prevWS)
     ]
+
     ++
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1, xK_2, xK_3, xK_4, xK_F1, xK_F2, xK_F3, xK_F4]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+
     ++
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        | (key, sc) <- zip [xK_a, xK_s, xK_d] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
-    -- mod-button1, Set the window to floating mode and move by dragging
+    -- mod-button3, Set the window to floating mode and move by dragging
     [ ((modm, button3), \w -> 
         focus w >>
         mouseMoveWindow w >>
         windows W.shiftMaster)
     , ((modm .|. shiftMask, button1), (\w -> XMonad.focus w >> mouseMoveWindow w
         >> windows W.shiftMaster))
+
     -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> XMonad.focus w >> windows W.shiftMaster))
-    -- mod-button3, Set the window to floating mode and resize by dragging
+
+    -- mod-button1, Set the window to floating mode and resize by dragging
     , ((modm, button1), (\w -> XMonad.focus w >> mouseResizeWindow w
         >> windows W.shiftMaster))
     ] 
@@ -150,6 +181,7 @@ main = do
      D.requestAccess dbus
      -- Start XMonad
      xmonad . ewmhFullscreen . ewmh $ myConfig { logHook = dynamicLogWithPP (myLogHook dbus)  } 
+
 myConfig = def {
   terminal = myTerminal
 , clickJustFocuses   = myClickJustFocuses
