2ce25863760999d21b151d7f10991d6d42437c5d
Alexander Etxeberria <89593545+TeDeJamaica@users.noreply.github.com>
2022-05-08 21:57:41 +0000

    Update lsp_config.lua

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -3,6 +3,8 @@ vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
+require'lspconfig'.tsserver.setup{}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
