49a68ddbb149e5bc6a728bc68a743d330500e516
Alexander Etxeberria <AlejandroEcheverria11@gmail.com>
2025-03-14 14:20:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D && sudo pacman-key --lsign-key 9AE4078033F8024D
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
 sudo cp ./pacman/pacman.conf /etc/
 
 sudo pacman -Syu efibootmgr networkmanager xclip dmenu dunst feh devilspie bc grub os-prober linux-lqx linux-lqx-headers yay lightdm lightdm-webkit2-greeter numlockx qutebrowser bspwm sxhkd alacritty ncmpcpp mpd zsh light $(pacman -Ssq xorg | grep -v git)
